00898cd782eb3be689fcda73050833bd5834d296
Patrick Morais <ppternunes@gmail.com>
2022-07-07 20:31:35 -0300

    feat: create UpdateProductUseCase

---
+++ src/modules/products/useCases/updateProduct/UpdateProductUseCase.ts
@@ -0,0 +1,35 @@
+import { Product } from '@modules/products/infra/typeorm/entities/Product';
+import { ProductRepository } from '@modules/products/infra/typeorm/repositories/ProductsRepository';
+import { AppError } from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class ShowProductUseCase {
+  async execute(data: IRequest): Promise<Product> {
+    const productRepository = getCustomRepository(ProductRepository);
+    const productExists = await productRepository.findOne(data.id);
+
+    if (!productExists) {
+      throw new AppError('Product not found', 404);
+    }
+
+    const productHasSameName = await productRepository.findByName(data.name);
+    if (productHasSameName && data.name !== productExists.name) {
+      throw new AppError('Product already exists', 400);
+    }
+
+    Object.assign(productExists, data);
+
+    await productRepository.save(productExists);
+
+    return productExists;
+  }
+}
+
+export { ShowProductUseCase };
