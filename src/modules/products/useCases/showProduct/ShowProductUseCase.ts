5665c6707ababf280d0eeea47fc19e9192a8ec2d
Patrick Morais <ppternunes@gmail.com>
2022-07-07 20:23:17 -0300

    feat: create ShowProductUseCase

---
+++ src/modules/products/useCases/showProduct/ShowProductUseCase.ts
@@ -0,0 +1,17 @@
+import { Product } from '@modules/products/infra/typeorm/entities/Product';
+import { ProductRepository } from '@modules/products/infra/typeorm/repositories/ProductsRepository';
+import { AppError } from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+class ShowProductUseCase {
+  async execute(id: string): Promise<Product> {
+    const productRepository = getCustomRepository(ProductRepository);
+    const productExists = await productRepository.findOne(id);
+    if (!productExists) {
+      throw new AppError('Product not found', 404);
+    }
+    return productExists;
+  }
+}
+
+export { ShowProductUseCase };
