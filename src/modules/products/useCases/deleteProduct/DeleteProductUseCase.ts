c9ab72bf95bcf240230e28e11c7afcafad0e13d1
Patrick Morais <ppternunes@gmail.com>
2022-07-07 20:34:53 -0300

    feat: create DeleteProductUseCase

---
+++ src/modules/products/useCases/deleteProduct/DeleteProductUseCase.ts
@@ -0,0 +1,16 @@
+import { ProductRepository } from '@modules/products/infra/typeorm/repositories/ProductsRepository';
+import { AppError } from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+class DeleteProductUseCase {
+  async execute(id: string): Promise<void> {
+    const productRepository = getCustomRepository(ProductRepository);
+    const productExists = await productRepository.findOne(id);
+    if (!productExists) {
+      throw new AppError('Product not found', 404);
+    }
+    await productRepository.remove(productExists);
+  }
+}
+
+export { DeleteProductUseCase };
