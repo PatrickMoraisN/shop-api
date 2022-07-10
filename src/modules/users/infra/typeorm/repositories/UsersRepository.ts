6d7947cd3931df2c9da113a43858b41aecccb3a7
Patrick Morais <ppternunes@gmail.com>
2022-07-10 15:51:32 -0300

    fix: fix UsersRepository

--- src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -4,7 +4,7 @@ import { User } from '../entities/Users';
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   public async findByName(name: string): Promise<User | undefined> {
-    const user = await this.usersRepository.findOne({
+    const user = await this.findOne({
       where: { name },
     });
 
@@ -12,21 +12,12 @@ export class UsersRepository extends Repository<User> {
   }
 
   public async findById(id: string): Promise<User | undefined> {
-    const user = await this.usersRepository.findOne({ where: { id } });
-
-    return user;
+    return this.findOne({ where: { id } });
   }
 
   public async findByEmail(email: string): Promise<User | undefined> {
-    const user = await this.usersRepository.findOne({ where: { email } });
+    const user = await this.findOne({ where: { email } });
 
     return user;
   }
-
-  constructor() {
-    super();
-    this.usersRepository = getRepository(User);
-  }
-
-  private usersRepository: Repository<User>;
 }
