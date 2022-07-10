8b67d93a1867ce5cd731fe720bebfe016f6b27c1
Patrick Morais <ppternunes@gmail.com>
2022-07-10 15:28:40 -0300

    feat: add users routes

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -2,11 +2,17 @@ import { Router } from 'express';
 import { CreateUserController } from '../useCases/createUser/CreateUserController';
 import { ListUsersController } from '../useCases/listUsers/ListUsersController';
 import { celebrate, Joi, Segments } from 'celebrate';
+import { DeleteUserController } from '../useCases/deleteUser/DeleteUserController';
+import { ShowUserController } from '../useCases/showUser/ShowUserController';
+import { UpdateUserController } from '../useCases/updateUser/UpdateUserController';
 
 const usersRouter = Router();
 
 const listUsersController = new ListUsersController();
 const createUserController = new CreateUserController();
+const deleteUserController = new DeleteUserController();
+const showUserController = new ShowUserController();
+const updateUserController = new UpdateUserController();
 
 usersRouter.get('/', listUsersController.handle);
 
@@ -20,6 +26,40 @@ usersRouter.post(
     },
   }),
   createUserController.handle,
+);
+
+usersRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().required(),
+    },
+  }),
+  deleteUserController.handle,
+);
+
+usersRouter.get('/', listUsersController.handle);
+
+usersRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string(),
+      email: Joi.string().email(),
+      password: Joi.string().min(6),
+    },
+  }),
+  updateUserController.handle,
+);
+
+usersRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().required(),
+    },
+  }),
+  showUserController.handle,
 );
 
 export { usersRouter };
