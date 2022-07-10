6fc73b59de27e96e25648a610ae8fb0aa33a7f20
Patrick Morais <ppternunes@gmail.com>
2022-07-10 15:53:19 -0300

    fix(ShowUserUseCase): calls getCustomRepository

--- src/modules/users/useCases/showUser/ShowUserUseCase.ts
+++ src/modules/users/useCases/showUser/ShowUserUseCase.ts
@@ -1,10 +1,11 @@
 import { User } from '@modules/users/infra/typeorm/entities/Users';
 import { UsersRepository } from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import { AppError } from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
 
 class ShowUserUseCase {
   async execute(id: string): Promise<User> {
-    const usersRepository = new UsersRepository();
+    const usersRepository = getCustomRepository(UsersRepository);
     const user = await usersRepository.findById(id);
 
     if (!user) {
