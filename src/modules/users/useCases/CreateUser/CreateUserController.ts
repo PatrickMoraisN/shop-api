2bcd7e36a549d0b4dbbb245cfcf10b34ebd303e5
Patrick Morais <ppternunes@gmail.com>
2022-07-09 21:13:40 -0300

    feat: create CreateUserController

---
+++ src/modules/users/useCases/CreateUser/CreateUserController.ts
@@ -0,0 +1,18 @@
+import { User } from '@modules/users/infra/typeorm/entities/Users';
+import { Request, Response } from 'express';
+import { CreateUserUseCase } from './CreateUserUseCase';
+
+class CreateUserController {
+  async handle(request: Request, response: Response): Promise<Response<User>> {
+    const { name, email, password } = request.body;
+    const createUserUseCase = new CreateUserUseCase();
+    const user = await createUserUseCase.execute({
+      name,
+      email,
+      password,
+    });
+    return response.json(user);
+  }
+}
+
+export { CreateUserController };
