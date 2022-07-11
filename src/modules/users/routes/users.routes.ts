fba63276cc8e4dcb2244d74c266726e01584413c
Patrick Morais <ppternunes@gmail.com>
2022-07-11 07:36:09 -0300

    feat(UsersRoutes): create route to update user avatar

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,21 +1,34 @@
 import { Router } from 'express';
+import multer from 'multer';
+import { celebrate, Joi, Segments } from 'celebrate';
+
 import { CreateUserController } from '../useCases/createUser/CreateUserController';
 import { ListUsersController } from '../useCases/listUsers/ListUsersController';
-import { celebrate, Joi, Segments } from 'celebrate';
 import { DeleteUserController } from '../useCases/deleteUser/DeleteUserController';
 import { ShowUserController } from '../useCases/showUser/ShowUserController';
 import { UpdateUserController } from '../useCases/updateUser/UpdateUserController';
+import { UpdateUserAvatarController } from '../useCases/updateUserAvatar/UpdateUserAvatarController';
 import { isAuthenticated } from '@shared/infra/http/middlewares/isAuthenticated';
+import uploadConfig from '@config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 const listUsersController = new ListUsersController();
 const createUserController = new CreateUserController();
 const deleteUserController = new DeleteUserController();
 const showUserController = new ShowUserController();
 const updateUserController = new UpdateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRouter.get('/', isAuthenticated, listUsersController.handle);
+
+usersRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  updateUserAvatarController.handle,
+);
 
 usersRouter.post(
   '/',
