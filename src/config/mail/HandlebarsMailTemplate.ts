14d5e29fc338a34a9b5d55818bb13f67129f308b
Patrick Morais <ppternunes@gmail.com>
2022-08-12 17:44:25 -0300

    refactor(HandlebarsMailTemplate): read file template

--- src/config/mail/HandlebarsMailTemplate.ts
+++ src/config/mail/HandlebarsMailTemplate.ts
@@ -1,12 +1,17 @@
 import handlebars from 'handlebars';
+import fs from 'fs';
 import { IParseMailTemplate } from './IParseMailTemplate';
 
 class HandlebarsMailTemplate {
   public async parse({
-    template,
+    file,
     variables,
   }: IParseMailTemplate): Promise<string> {
-    const parseTemplate = handlebars.compile(template);
+    const templateFileContent = await fs.promises.readFile(file, {
+      encoding: 'utf-8',
+    });
+
+    const parseTemplate = handlebars.compile(templateFileContent);
 
     return parseTemplate(variables);
   }
