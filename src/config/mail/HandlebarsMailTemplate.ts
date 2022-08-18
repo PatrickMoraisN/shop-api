69c3828c6e3ee302ab5260cba7667e361684bfc4
Patrick Morais <ppternunes@gmail.com>
2022-08-18 15:02:26 -0300

    fix(HandlebarsMailTemplate): fix typo

--- src/config/mail/HandlebarsMailTemplate.ts
+++ src/config/mail/HandlebarsMailTemplate.ts
@@ -3,10 +3,7 @@ import fs from 'fs';
 import { IParseMailTemplate } from './IParseMailTemplate';
 
 class HandlebarsMailTemplate {
-  public async parse({
-    file,
-    variables,
-  }: IParseMailTemplate): Promise<string> {
+  public async parse({ file, variables }: IParseMailTemplate): Promise<string> {
     const templateFileContent = await fs.promises.readFile(file, {
       encoding: 'utf-8',
     });
