d0f09b7cbb7a8c8a9e651a3f330142437cedfc6d
Patrick Morais <ppternunes@gmail.com>
2022-08-30 22:48:10 -0300

    feat(ProductsRepository): create method findAllByIds

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -1,5 +1,9 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, In, Repository } from 'typeorm';
 import { Product } from '../entities/Product';
+
+interface IFindProducts {
+  id: string;
+}
 
 @EntityRepository(Product)
 export class ProductRepository extends Repository<Product> {
@@ -9,5 +13,19 @@ export class ProductRepository extends Repository<Product> {
         name,
       },
     });
+  }
+
+  async findAllByIds(
+    products: IFindProducts[],
+  ): Promise<Product[] | undefined> {
+    const productsIds = products.map(product => product.id);
+
+    const productsExists = await this.find({
+      where: {
+        id: In(productsIds),
+      },
+    });
+
+    return productsExists;
   }
 }
